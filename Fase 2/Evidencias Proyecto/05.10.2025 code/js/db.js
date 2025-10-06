78c7f06c7deaabee0e818973173c18a0c7012db5
cedreMC <cedr3.oss@gmail.com>
2025-10-06 14:53:15 -0300

    05.10.2025 code

---
+++ Fase 2/Evidencias Proyecto/05.10.2025 code/js/db.js
@@ -0,0 +1,21 @@
+// db.js
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost', // Cambiar si usas un servidor remoto
+  user: 'root',      // Tu usuario de MySQL
+  password: '1@d&838VyO',      // Tu contraseña de MySQL
+  database: 'myweb' // El nombre de tu base de datos
+});
+
+// Conectar a la base de datos
+db.connect((err) => {
+  if (err) {
+    console.error('Error de conexión a MySQL:', err);
+  } else {
+    console.log('Conexión exitosa a MySQL');
+  }
+});
+
+module.exports = db; // Exportamos la conexión para usarla en app.js
+
